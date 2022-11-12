3be6a4156758338b1616c4b0faff5c8c2acad14d
Vika Kovalenko <shmoe761@gmail.com>
2022-11-12 22:22:35 +0200

    + enum Status created; + Answer.java: added a new variable; + cards.yml edited

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -1,6 +1,11 @@
-public class Answer extends Sentence {
+import lombok.Getter;
 
-    public Answer(int id, String context) {
+public class Answer extends Sentence {
+    @Getter
+    private final Status status;
+
+    public Answer(int id, String context, Status status) {
         super(id, context);
+        this.status = status;
     }
 }
---
+++ src/main/java/Status.java
@@ -0,0 +1,5 @@
+public enum Status {
+    NEXT,
+    DEFEAT,
+    VICTORY
+}
