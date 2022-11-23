5a1023023ed13a4e440c067532550cdb24ecf8b2
Vika Kovalenko <shmoe761@gmail.com>
2022-11-23 20:15:48 +0200

    Card.java: added a new variable, changed type of "answers" variable; cards.yml edited

--- src/main/java/kovalenko/vika/basis/Card.java
+++ src/main/java/kovalenko/vika/basis/Card.java
@@ -2,16 +2,21 @@ package kovalenko.vika.basis;
 
 import lombok.Getter;
 
+import java.util.List;
+
 public class Card {
+    @Getter
+    private Integer id;
     @Getter
     private Question question;
     @Getter
-    private Answer[] answers;
+    private List<Answer> answers;
 
     public Card() {
     }
 
-    public Card(Question question, Answer[] answers) {
+    public Card(Integer id, Question question, List<Answer> answers) {
+        this.id = id;
         this.question = question;
         this.answers = answers;
     }
