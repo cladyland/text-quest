c9eaa93fad9344980c7facf41accc5d6ccb74e9b
Vika Kovalenko <shmoe761@gmail.com>
2022-11-26 18:19:41 +0200

    + PlayerSettingsException created

---
+++ src/main/java/kovalenko/vika/service/exception/PlayerSettingsException.java
@@ -0,0 +1,8 @@
+package kovalenko.vika.service.exception;
+
+public class PlayerSettingsException extends RuntimeException {
+
+    public PlayerSettingsException(String message) {
+        super(message);
+    }
+}
